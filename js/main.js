4ee5eef72b178f648cca175a52fe84c3170974f5
pujaKundu <pujakundu797@gmail.com>
2024-02-01 17:37:56 +0600

    about section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-const navToggleCheckbox = document.querySelector('#navigation-toggle');
-const navBtn = document.querySelector('.navigation__button');
+const navToggleCheckbox = document.querySelector("#navigation-toggle");
+const navBtn = document.querySelector(".navigation__button");
 
-navBtn.addEventListener('click', ()=> {
-    console.log('clicked')
-    if (navToggleCheckbox.checked) {
-        navToggleCheckbox.setChecked = false;
-    } else {
-        navToggleCheckbox.setChecked = true;
-    }
+navBtn.addEventListener("click", () => {
+  if (navToggleCheckbox.checked) {
+    navToggleCheckbox.setChecked = false;
+  } else {
+    navToggleCheckbox.setChecked = true;
+  }
 });
-
-
