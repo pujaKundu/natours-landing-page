635df9c3cfef3807e18af86a7c9537d3481c9ad5
pujaKundu <pujakundu797@gmail.com>
2024-02-05 15:01:51 +0600

    popup

--- js/main.js
+++ js/main.js
@@ -8,3 +8,25 @@ navBtn.addEventListener("click", () => {
     navToggleCheckbox.setChecked = true;
   }
 });
+
+
+
+document.getElementById('openPopup').addEventListener('click', function(event) {
+  
+  event.preventDefault();
+
+  var popup = document.getElementById('popup');
+
+  popup.style.opacity = 1;
+  popup.style.visibility = 'visible';
+  document.body.classList.add('popup-open');
+});
+
+
+document.querySelector('.popup__close').addEventListener('click', function() {
+  var popup = document.getElementById('popup');
+  popup.style.opacity = 0;
+  popup.style.visibility = 'hidden';
+
+  document.body.classList.remove('popup-open');
+});
